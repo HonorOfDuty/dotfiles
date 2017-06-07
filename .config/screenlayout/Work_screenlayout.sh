87d28695c438aec03e8e441e87c848770796c304
HonorOfDuty <c.vossen@protonmail.com>
2017-06-07 15:59:38 +0200

    Create Work_screenlayout.sh
    Screenlayout for laptop at work. Laptop in front, screen above laptop screen.

---
+++ .config/screenlayout/Work_screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 280x1080 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
